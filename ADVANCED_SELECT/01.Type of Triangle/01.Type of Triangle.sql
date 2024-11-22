cdeb70041e7fea05dfe4bf9b22aab8d1c1880078
Aslı Kuşçu <115110503+aslikuscu@users.noreply.github.com>
2024-11-22 12:49:56 +0300

    Create 01.Type of Triangle.sql

---
+++ ADVANCED_SELECT/01.Type of Triangle/01.Type of Triangle.sql
@@ -0,0 +1,13 @@
+SELECT
+   IF(A+B>C AND A+C>B AND B+C>A,
+     IF(A=B AND B=C, 'Equilateral',
+       IF(A=B OR B=C OR A=C, 'Isosceles', 'Scalene')
+       ),
+      'Not A Triangle'
+     )
+FROM TRIANGLES;
+
+--TRIANGLE = UCGEN
+--Isosceles = IKIZKENAR
+--Equilateral = ESKENAR
+--Scalene = CESITKENAR
